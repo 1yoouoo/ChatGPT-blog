d511ee730c40e2cc0f8afd7ac750c935a7a28893
blanc <1yoouoo@gmail.com>
2023-06-15 19:16:05 +0900

    final

--- publish.sh
+++ publish.sh
@@ -1,24 +1,23 @@
-# # active python
-# echo "started"
-# cd /home/yoon/blog/1yoouoo.github.io
-# /usr/bin/python3 main.py >> /home/yoon/blog/1yoouoo.github.io/main.log 2>&1 
-
-# git pull
-# git config --local user.name "blanc"
-# git config --local user.email "1yoouoo@gmail.com" 
-# sleep 0.1
-# git add -A
-# git commit -m "publish blog"
-# git push origin HEAD
-
-
-# active python
-cd /Users/blanc/Documents/ChatGPT/ChatGPT-blog
-/opt/homebrew/bin/python3 main.py >> /Users/blanc/Documents/ChatGPT/ChatGPT-blog/main.log 2>&1
+# # active python on linux
+echo "started"
+cd /home/yoon/ChatGPT/ChatGPT-blog
+/usr/bin/python3 main.py >> /home/yoon/ChatGPT/ChatGPT-blog/main.log 2>&1 
 
 git config --local user.name "blanc"
 git config --local user.email "1yoouoo@gmail.com" 
 sleep 0.1
 git add -A
 git commit -m "publish blog"
-git push origin head
+git push origin HEAD
+
+
+# active python on mac
+# cd /Users/blanc/Documents/ChatGPT/ChatGPT-blog
+# /opt/homebrew/bin/python3 main.py >> /Users/blanc/Documents/ChatGPT/ChatGPT-blog/main.log 2>&1
+
+# git config --local user.name "blanc"
+# git config --local user.email "1yoouoo@gmail.com" 
+# sleep 0.1
+# git add -A
+# git commit -m "publish blog"
+# git push origin HEAD
--- start.sh
+++ start.sh
@@ -1,16 +1,22 @@
 echo "started"
 
 # 브랜치 이름 배열
-BRANCHES=("nextjs" "javascript" "react")
+BRANCHES=("nextjs" "javascript" "react" "python" "java" "spring" "vue" "redux")
 
 # 배열에서 랜덤하게 브랜치 이름 선택
 BRANCH=${BRANCHES[$RANDOM % ${#BRANCHES[@]}]}
 
-cd /Users/blanc/Documents/ChatGPT/ChatGPT-blog
-# 선택한 브랜치로 checkout
+# active python on linux
+cd /home/yoon/ChatGPT/ChatGPT-blog
+
+# active python on mac
+# cd /Users/blanc/Documents/ChatGPT/ChatGPT-blog
+
 
 echo "$BRANCH 입니다."
 
 git checkout $BRANCH
-
+sleep 0.1
 ./publish.sh
+
+
