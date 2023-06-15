8a25bb030430dcbbc5f41522f77aa6b32b90900e
blanc <1yoouoo@gmail.com>
2023-06-15 18:55:01 +0900

    nextjs

--- publish.sh
+++ publish.sh
@@ -11,15 +11,14 @@
 # git commit -m "publish blog"
 # git push origin HEAD
 
+
 # active python
-echo "started"
-cd /Users/blanc/Documents/ChatGPT/1yoouoo.github.io
+cd /Users/blanc/Documents/ChatGPT/ChatGPT-blog
 /opt/homebrew/bin/python3 main.py >> /Users/blanc/Documents/ChatGPT/ChatGPT-blog/main.log 2>&1
 
-git pull
 git config --local user.name "blanc"
 git config --local user.email "1yoouoo@gmail.com" 
 sleep 0.1
 git add -A
 git commit -m "publish blog"
-git push origin HEAD
+git push origin head
